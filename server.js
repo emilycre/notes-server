2e0f5e7d3046bd22a5257b3453dc8b0668c35ec1
Emily Creamer <emily.creamer@icloud.com>
2019-06-11 13:42:12 -0700

    Forgot route in server.js...

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
 
+app.use('/api/v1/notes', require('./lib/routes/notes'));
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Listening on ${PORT}`);
